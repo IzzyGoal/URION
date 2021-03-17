5ea4e90d632edca4a2782f07677e4ee4e3f864a4
IzzyGoal <gabriel.osilvalima@gmail.com>
2021-03-17 20:42:03 +0000

    problem in code whit %

--- Main.java
+++ Main.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
-class Main {
-public static void main(String[] args){
-Scanner entrada = new Scanner(System.in);
-  int valor = entrada.nextInt();
-System.out.println(valor);
-  System.out.println(valor / 100 +" nota(s) de R$ 100,00");
-  valor = valor%100;
-    System.out.println(valor / 50 +" nota(s) de R$ 50,00");
-    valor = valor%50;
-      System.out.println(valor / 20 +" nota(s) de R$ 20,00");
-      valor = valor%20;
-        System.out.println( valor / 10+" nota(s) de R$ 10,00");
-        valor = valor%10;
-          System.out.println(valor / 5 +" nota(s) de R$ 5,00");
-          valor = valor%5;
-            System.out.println(valor / 2 +" nota(s) de R$ 2,00");
-            valor = valor%2;
-              System.out.println(valor / 1+" nota(s) de R$ 1,00");     
+class Main{
+public static void main(String [] args){
+    Scanner entrada = new Scanner(System.in);  
+    //DecimalFormat formatado = new DecimalFormat("0");
+     Double Valor = entrada.nextDouble();
+  System.out.println("NOTAS:");
+  System.out.println(Valor/100+" nota(s) de R$ 100.00");
+    Valor = Valor%100;
+  System.out.println(Valor/50+" nota(s) de R$ 50,00");
+    Valor = Valor%50;
+  System.out.println(Valor/20+" nota(s) de R$ 20.00");
+    Valor=Valor%20; 
+  System.out.println(Valor/10+" nota(s) de R$ 10.00");
+    Valor=Valor%10;
+  System.out.println(Valor/5+" nota(s) de R$ 5,00");
+    Valor=Valor%5;
+  System.out.println(Valor/2 +" nota(s) de R$ 2,00");
+    Valor = Valor%2;
+    System.out.println("MOEDAS:");
+  System.out.println(Valor/1 +" moeda(s) de R$ 1.00");  
+    Valor = Valor%1;
+  System.out.println(Valor/0.5 +" moeda(s) de R$ 0.50");
+    Valor = Valor%0.5;
+  System.out.println(Valor/0.25 +" moeda(s) de R$ 0,25");
+    Valor = Valor%0.25;
+  System.out.println(Valor/0.10+" moeda(s) de R$ 0.10");
+    Valor=Valor%0.10;
+  System.out.println(Valor/0.05 +" moeda(s) de R$ 0,05");
+    Valor=Valor%0.05;
+  System.out.println(Valor/0.01 +" moeda(s) de R$ 0,01");
 }
 }
